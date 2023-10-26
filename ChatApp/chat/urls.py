1606676fbe5b7ca2c5bc90ae137a0a9dc7c17239
SabaFadhl <sabafadl98@gmai.com>
2023-10-26 21:40:08 +0300

    feat: sign up interface

--- ChatApp/chat/urls.py
+++ ChatApp/chat/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from chat import views as chat_views
 from django.contrib.auth.views import LoginView, LogoutView
-
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from .views import SignUpView
 
 urlpatterns = [
 	path("", chat_views.chat_page, name="chat-page"),
 	# login-section
 	path("auth/login/", LoginView.as_view(template_name="chat/LoginPage.html"), name="login-user"),
 	path("auth/logout/", LogoutView.as_view(), name="logout-user"),
+	path("register/", SignUpView.as_view(), name="create-user"),
 ]
--- ChatApp/chat/views.py
+++ ChatApp/chat/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 
 def chat_page(request, *args, **kwargs):
@@ -10,3 +14,10 @@ def chat_page(request, *args, **kwargs):
 		return redirect("login-user")
 	context = {}
 	return render(request, "chat/chatPage.html", context)
+
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login-user")
+    template_name = "chat/Signup.html"
